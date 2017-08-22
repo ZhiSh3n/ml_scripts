1cf3d68ce7ea1ffa4b9f33239dc81236326795bd
zhishenyong@live.com <zhishenyong@live.com>
2017-08-22 19:20:40 +0800

    ported the manual mnist over to catdog

--- keras_tensorflow/catdog/run.py
+++ keras_tensorflow/catdog/run.py
@@ -10,70 +10,85 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD,RMSprop,adam
 from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import load_model
+from keras.models import model_from_json
 
-# dimensions of the images
-image_width, image_height = 28, 28
 
-# data locations
+# all images should be put into /data
+# here we set main_directory to be the path for the images
 PATH = os.getcwd()
-main_directory = PATH + '/mnist_png'
+main_directory = PATH + '/data'
 directory_list = os.listdir(main_directory)
 
+
 # training parameters
-epochs = 1
-batch_size = 128
-number_classes = 10
-#input_shape = (image_width, image_height, 1)
+# change these as your evaluate the model
+epochs = 50
+batch_size = 16
 
-
-# get our data
+# other data
 image_data_list = []
 class_population = []
+number_classes = 0
+# dimensions of the images
+image_width, image_height = 200, 200
+
+# loop to get data
 for dataset in directory_list :
-    counter = 0
     if os.path.isdir(main_directory + '/' + dataset) :
+        counter = 0
+        # alternatively, you can put counter above the for loop
+        # you will need to edit the split loop to account for this
+        number_classes = number_classes + 1
         image_list = os.listdir(main_directory + '/' + dataset)
         print ('Loaded the images of dataset: '+'{}\n'.format(dataset))
         for image in image_list:
-            if image.endswith('.png') :
+            if image.endswith('.jpg') :
                 counter = counter + 1
                 input_image = cv2.imread(main_directory + '/' + dataset + '/' + image)
                 # left out resizing because we are doing mnist.
                 # may need to add resizing if doing other photos
+                # debate whether you need to change to grayscale or not
                 input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY) #changing to grayscale
                 input_image_resize = cv2.resize(input_image,(image_width, image_height))
                 image_data_list.append(input_image_resize)
         class_population.append(counter)
 number_samples = len(image_data_list)
-print('Total number of images loaded:', number_samples)
+print('Found a total of' , number_classes , 'classes with', number_samples, 'total samples.')
+print('')
+
 
 # misc
 image_data = np.array(image_data_list)
 image_data = image_data.astype('float32')
 image_data /= 255
-print ('Shape of [image_data]:', image_data.shape)
+print('Shape of image_data:', image_data.shape)
+print('')
 image_data= np.expand_dims(image_data, axis=4) # adding extra dim
 
 
 # split our data
-print(class_population)
+print('Class population:', class_population)
+print('')
+print('Splitting data...')
 labels = np.ones((number_samples,),dtype='int64')
-# you can probably add a for loop here
-
-for first_counter in range(0,10) :
+for first_counter in range(0,number_classes) :
     if first_counter == 0 :
         # the first class_population[0] number of samples are 0
         labels[0:class_population[first_counter]] = 0
     else :
-        if first_counter == 9 :
-            final_sum = sum(class_population) - class_population[9]
-            labels[final_sum:] = 9
+        if first_counter == (number_classes - 1) :
+            final_sum = sum(class_population) - class_population[number_classes - 1]
+            labels[final_sum:] = (number_classes - 1)
         else :
             small_sum = 0
             big_sum = 0
@@ -82,7 +97,7 @@ for first_counter in range(0,10) :
             small_sum = big_sum - class_population[first_counter]
             labels[small_sum:big_sum] = first_counter
         
-        
+print('Splitting data finished!')       
 """
 labels[0:class_population[0]] = 0
 labels[class_population[0]:class_population[1]] = 1
@@ -106,7 +121,6 @@ print(labels[59500])
 print(labels[66500])
 """
 
-names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 Y = np_utils.to_categorical(labels, number_classes)
 x, y = shuffle(image_data, Y, random_state = 2)
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
@@ -118,7 +132,8 @@ X_test = X_test.reshape(X_test.shape[0], image_width, image_height, 1)
 input_shape = image_data[0].shape
 print(image_data[0].shape)
 
-
+"""
+# personal model used successfully with mnist
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
@@ -130,30 +145,96 @@ model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(number_classes, activation='softmax'))
-
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
-
 model.fit(X_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(X_test, y_test))
+"""
+
+model = Sequential()
+# input: 100x100 images with 3 channels -> (3, 100, 100) tensors.
+# this applies 32 convolution filters of size 3x3 each.
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.25))
+
+#model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+# Note: Keras does automatic shape inference.
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(number_classes))
+model.add(Activation('softmax'))
+"""
+# this one is the best visual recog, but doesn't show accuracy
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
+model.fit(X_train, y_train, batch_size=32, epochs=epochs, verbose=1, validation_data=(X_test, y_test))
+"""
+
+# this is the mnist model
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+model.fit(X_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(X_test, y_test))
+
+# score
 score = model.evaluate(X_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-"""
-add an area here for saving and loading so we don't have to train every time
+# save model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
 """
 
-for filename in os.listdir("mnist_test"):
+# laod model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+
+
+# use loaded model
+loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+for filename in os.listdir("???"):
     if filename.endswith('.png'):
-        img = cv2.imread("mnist_test/%s" % filename)
+        img = cv2.imread("???/%s" % filename)
         img = img[:,:,0]
         img = np.expand_dims(img,2)
         img.shape = (1,28,28,1)
-        prediction = model.predict(img)
+        prediction = loaded_model.predict(img)
         print(prediction)
-
+"""
